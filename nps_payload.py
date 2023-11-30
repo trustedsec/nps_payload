02455e885c26af7d660d5240d8208a810ed3cbd4
donadelden <50483408+donadelden@users.noreply.github.com>
2023-11-30 10:24:07 +0100

    Fixed the fix for xml file is null #5

--- nps_payload.py
+++ nps_payload.py
@@ -178,7 +178,8 @@ def generate_msbuild_nps_msf_payload():
       </Code>
     </Task>
   </UsingTask>
-</Project>""" % psh_payload).close()
+</Project>""" % psh_payload)
+  msbuild_nps_file.close()
 
   print(bcolors.GREEN + "[+]" + bcolors.ENDC + " Metasploit resource script written to msbuild_nps.rc")  
   print(bcolors.GREEN + "[+]" + bcolors.ENDC + " Payload written to msbuild_nps.xml")
